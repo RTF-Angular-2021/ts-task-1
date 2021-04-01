f6e477911f38273e04780fb230401bb54a80360a
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2021-04-01 12:41:39 +0500

    Complete

--- src/task_4/index.ts
+++ src/task_4/index.ts
@@ -12,3 +12,57 @@ type FooType = {
     a: number
     b: string
 };
+
+/*
+Приведу аналогию, пожалуй. Type это struct в C#, только не имеющий инициализатора/конструктора
+Class can implement Interface, types
+Class can be extended by Interface and classes
+Variable can implement type
+Variable cannot be extended by any of type or interface
+*/
+
+interface IAnimal{
+    DoSound(): void
+    name: string
+    sex: string //better enum
+}
+
+type DogType ={
+    breed: string;
+    name: string|Number;
+    doSound(): void;
+}
+
+class Dog implements IAnimal{
+    DoSound(): void {
+        console.log("Whoooff!");
+    }
+    name: "Axel";
+    sex: "Male";
+    breed: "HolyWater";
+}
+
+//But cannot compile class when extending type
+
+class DogT extends DogType{ //Compile error
+    doSound(): void {
+        throw new Error("Method not implemented.");
+    }
+    breed: string;
+    name: 21312;
+    
+}
+
+//At the same time, can compile class when implementing type
+
+class DogTI implements DogType{
+    doSound(): void {
+        throw new Error("Method not implemented.");
+    }
+    breed: string;
+    name: 21312;
+    
+}
+
+//type cannot be extended by another type or interface
+//Interface can be extended by another interface
