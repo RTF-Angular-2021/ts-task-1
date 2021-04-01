1e51b889de3ab4556d7c3c4ded5a3685b3305063
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-04-01 15:48:00 +0500

    Бадалбейли Эльдар

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -3,8 +3,16 @@
  * который написан в функции logObj компилировался и исполнялся корректно
 */
 
-type FooType = unknown;
-type BarType = unknown;
+type FooType = {
+    stringProp: string, 
+    numberProp: number, 
+    barObject: BarType
+};
+type BarType = {
+    stringsArrayProp: Array<string>, 
+    numbersOrDatesArrayProp: Array<Date | number>, 
+    functionProp: (flag: boolean) => void
+};
 
 export const fooObjects: FooType[] = [
     {
--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -7,6 +7,15 @@
 */
 function add(x: string, y: string): string;
 function add(x: number, y: number): number;
+function add(x: string | number, y: string | number): string | number {
+    if (typeof x === 'string' && typeof y === 'string'){
+        return x + y;
+    } else if (typeof x === 'number' && typeof y === 'number'){
+        return x + y;
+    } else{
+        throw new Error("Неверный тип данных у передаваемого параметра");
+    }
+}
 
 add('20', '21'); //2021
 add(20, 21); //41
